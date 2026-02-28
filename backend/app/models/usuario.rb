44c1dd8c48f9e4296a7316399c6d23c9a8e88fcc
Mateus De Vita <mateusdevitata@gmail.com>
2026-02-28 15:36:06 -0300

    feat: configura banco de dados e implementa migration e model para Usuarios

--- backend/app/models/usuario.rb
+++ backend/app/models/usuario.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+class Usuario < ApplicationRecord
+  has_secure_password
+
+  validates :nome, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  before_validation :normalizar_email
+
+  def as_json(options = {})
+    super(only: %i[id nome email created_at], **options)
+  end
+
+  private
+
+  def normalizar_email
+    self.email = email&.downcase&.strip
+  end
+end
