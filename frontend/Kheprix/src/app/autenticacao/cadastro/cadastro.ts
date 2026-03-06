280c34ef06f95ff2365b3ade2dff9556944333e5
Yas Tainá <yas.taina.silva@gmail.com>
2026-03-06 01:04:54 -0300

    Auth models e Base Services

--- frontend/Kheprix/src/app/autenticacao/cadastro/cadastro.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cadastro',
-  imports: [],
-  templateUrl: './cadastro.html',
-  styleUrl: './cadastro.css',
-})
-export class Cadastro {}
