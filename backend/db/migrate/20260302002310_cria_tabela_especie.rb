ea4afe61482d6d156e549f4823a05c29363dd2bc
Qtaro-Kujo <joao_albertop@hotmail.com>
2026-03-21 22:19:47 -0300

    fix: corrige campos dos endpoints de especie

--- backend/db/migrate/20260302002310_cria_tabela_especie.rb
+++ backend/db/migrate/20260302002310_cria_tabela_especie.rb
@@ -7,16 +7,18 @@ class CriaTabelaEspecie < ActiveRecord::Migration[8.0]
 
       t.string :foto                                          # URL da foto (opcional)
       t.string :classe, limit: 100                            # Classe taxonômica (opcional)
+      t.string :ordem, limit: 100                             # Ordem taxonômica (opcional)
+      t.string :familia, limit: 100                           # Família taxonômica (opcional)
       t.string :genero, limit: 100                            # Gênero taxonômico (opcional)
+      t.string :especie, limit: 100                            # Espécie taxonômica (opcional)
       t.string :nome_popular                                  # Nome popular (opcional)
-      t.string :nome_cientifico                               # Nome científico (opcional)
       t.string :status_conservacao, limit: 100                # Ex.: "Em perigo", "Vulnerável" (opcional)
-      t.boolean :nativa_da_regiao, default: false             # Default false
+      t.boolean :endemismo, default: false                    # Endêmica da região?
 
       t.timestamps
     end
 
-    add_index :especies, %i[estudo_id nome_cientifico], unique: true
+    add_index :especies, %i[estudo_id especie], unique: true
     add_foreign_key :especies, :estudos
   end
 end
