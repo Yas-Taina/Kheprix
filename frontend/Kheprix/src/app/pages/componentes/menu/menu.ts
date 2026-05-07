1e2a2156eadf5f559e4dce56eec136e951208215
Christian-Eurinidio <113392481+Christian-Eurinidio@users.noreply.github.com>
2026-05-06 21:58:10 -0300

    ajustes frontend

--- frontend/Kheprix/src/app/pages/componentes/menu/menu.ts
+++ frontend/Kheprix/src/app/pages/componentes/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   standalone: true,
@@ -10,8 +11,19 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./menu.css'],
 })
 export class Menu {
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {}
   isOpen = false;
   toggleMenu(){
     this.isOpen = !this.isOpen
   }
+
+  fazerLogout() {
+    this.authService.logout();
+  }
+
+  abrirHome() { this.router.navigate(['/inicio']); }
+  abrirRegistroRapido()   { this.router.navigate(['/registro-rapido']); }
+  abrirVisualizarEstudos() { this.router.navigate(['/estudos']); }
+  abrirCadastrarNovoEstudo()    { this.router.navigate(['/estudos/novo']); }
+  abrirConvitesColaboracao()      { this.router.navigate(['/convites']); }
 }
