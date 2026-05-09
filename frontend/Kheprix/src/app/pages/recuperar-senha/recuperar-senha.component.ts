736844d79b3f66da4cf072ffac503c290bd06eb7
Yas Tainá <yas.taina.silva@gmail.com>
2026-05-09 03:29:07 -0300

    Responsividade, fallback e icones em páginas de Auth

--- frontend/Kheprix/src/app/pages/recuperar-senha/recuperar-senha.component.ts
+++ frontend/Kheprix/src/app/pages/recuperar-senha/recuperar-senha.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { AuthService } from '../../core/services/auth.service';
   standalone: true,
   templateUrl: './recuperar-senha.component.html',
   styleUrls: ['./recuperar-senha.component.css'],
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule],
 })
 export class RecuperarSenhaComponent {
   email = ''; token = ''; novaSenha = ''; confirmarSenha = '';
