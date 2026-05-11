8f5a5380de8f183314c322d9a6a19e945ff42e21
Yas Tainá <yas.taina.silva@gmail.com>
2026-05-10 22:41:29 -0300

    Váriaveis em editar registro

--- frontend/Kheprix/src/app/pages/estudos/novo/estudo-novo.component.ts
+++ frontend/Kheprix/src/app/pages/estudos/novo/estudo-novo.component.ts
@@ -3,8 +3,7 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
 import { EstudoService } from "../../../core/services/estudo.service";
-import { VariavelService } from "../../../core/services/variavel.service";
-import { VariavelCreate, Variavel } from "../../../models";
+import { VariavelCreate } from "../../../models";
 
 interface VariavelForm extends VariavelCreate {
   _id: number;
@@ -17,43 +16,19 @@ interface VariavelForm extends VariavelCreate {
   styleUrls: ["./estudo-novo.component.css"],
   imports: [CommonModule, FormsModule],
 })
-export class EstudoNovoComponent implements OnInit {
+export class EstudoNovoComponent {
   nome = "";
   observacoes = "";
   variaveis: VariavelForm[] = [];
-  variaveisEdit: Variavel[] = [];
-  isEdit = false;
-  estudoId: number | null = null;
   loading = false;
   erro = "";
   private _counter = 0;
 
   constructor(
     private estudoService: EstudoService,
-    private variavelService: VariavelService,
     public router: Router,
     private route: ActivatedRoute,
   ) {}
-
-  ngOnInit(): void {
-    this.estudoId = this.route.snapshot.params["estudo_id"]
-      ? +this.route.snapshot.params["estudo_id"]
-      : null;
-    this.isEdit = !!this.estudoId;
-
-    if (this.isEdit && this.estudoId) {
-      this.estudoService.listar().subscribe((lista) => {
-        const estudo = lista.find((e) => e.id === this.estudoId);
-        if (estudo) {
-          this.nome = estudo.nome;
-          this.observacoes = estudo.observacoes ?? "";
-        }
-      });
-      this.variavelService.listar(this.estudoId).subscribe((vars) => {
-        this.variaveisEdit = vars;
-      });
-    }
-  }
 
   addVariavel(): void {
     this.variaveis.push({
@@ -77,20 +52,7 @@ export class EstudoNovoComponent implements OnInit {
     this.loading = true;
     this.erro = "";
 
-    if (this.isEdit && this.estudoId) {
-      this.estudoService
-        .atualizar(this.estudoId, {
-          nome: this.nome,
-          observacoes: this.observacoes,
-        })
-        .subscribe({
-          next: () => this.router.navigate(["/estudos", this.estudoId]),
-          error: () => {
-            this.erro = "Erro ao salvar.";
-            this.loading = false;
-          },
-        });
-    } else {
+
       const payload = {
         nome: this.nome,
         observacoes: this.observacoes,
@@ -103,6 +65,5 @@ export class EstudoNovoComponent implements OnInit {
           this.loading = false;
         },
       });
-    }
   }
 }
